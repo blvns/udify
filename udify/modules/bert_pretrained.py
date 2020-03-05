55e3b686622c8f0bc68a6809e74232992fc11a45
Terra Blevins <blvns@cs.washington.edu>
2020-03-04 23:47:29 -0800

    manually handling embeddings

--- udify/modules/bert_pretrained.py
+++ udify/modules/bert_pretrained.py
@@ -522,8 +522,6 @@ class BertEmbedder(TokenEmbedder):
         #run embeddings through bert encoders
         all_encoder_layers = self.bert_model.encoder(embedded_inputs,
                                                 attention_mask=attn_mask)
-        print(len(all_encoder_layers), all_encoder_layers[0].shape)
-        quit()
         all_encoder_layers = torch.stack(all_encoder_layers)
 
         if needs_split:
