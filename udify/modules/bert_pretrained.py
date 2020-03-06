46d1d7db174c440477eefd10c4c8fd9203fe2a78
Terra Blevins <blvns@cs.washington.edu>
2020-03-05 22:14:57 -0800

    perturbed training -- shuffle positional embeddings

--- udify/modules/bert_pretrained.py
+++ udify/modules/bert_pretrained.py
@@ -530,7 +530,6 @@ class BertEmbedder(TokenEmbedder):
         #embedded_inputs = embedded_words + embedded_positions + embedded_types
 
         #PERTURBED finetuning (rotate positional embeddings)
-        print(position_ids.shape)
         p_len = position_ids.shape[-1]
         split_position_ids = list(position_ids.split(1, dim=0))
         for idx in range(len(split_position_ids)): 
@@ -538,8 +537,6 @@ class BertEmbedder(TokenEmbedder):
             rotate_offset = random.randint(0, p_len-1)
             split_position_ids[idx] = torch.cat([p[:, p_len-rotate_offset:], p[:, :p_len-rotate_offset]], dim=-1)
         position_ids = torch.cat(split_position_ids, dim=0)
-        print(position_ids.shape)
-        quit()
         embedded_positions = self.bert_model.embeddings.position_embeddings(position_ids)
         embedded_inputs = embedded_words + embedded_positions + embedded_types
 
