46f4e53098098710b0c02ef1f03e09f23b5f0dbb
Terra Blevins <blvns@cs.washington.edu>
2020-03-10 21:50:26 -0700

    getting syntax encoders for probing

---
+++ extract_syntax_encoder.py
@@ -0,0 +1,31 @@
+from allennlp.models.archival import load_archive
+from allennlp.predictors.predictor import Predictor
+from allennlp.common.util import import_submodules
+
+import argparse
+import sys
+sys.path.append('/private/home/tblevins/udify/')
+
+parser = argparse.ArgumentParser(description='Extract BERT encoder from UDify model')
+parser.add_argument('--ckpt', type=str, required=True,
+	help='location of AllenNLP model archive')
+
+def main(args):
+	#UDify setup (from predict.py file)
+	import_submodules("udify")
+
+	predictor = "udify_predictor"
+
+	#load model
+	archive = load_archive(args.ckpt) #cuda_device=cuda_device)
+	predictor = Predictor.from_archive(archive, predictor)
+
+	print(predictor)
+
+
+if __name__ == "__main__":
+	args = parser.parse_args()
+	print(args) 
+	main(args)
+
+#EOF
